20d9300c5e0960ec29256989c5721970a2bc2070
Andrey Pavlov <andrey-mp@yandex.ru>
2016-08-23 12:26:19 +0300

    add comments to scripts

--- create_env.sh
+++ create_env.sh
@@ -3,15 +3,26 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
+# instructions was used:
+#  https://keithtenzer.com/2015/10/14/howto-openstack-deployment-using-tripleo-and-the-red-hat-openstack-director/
+#  http://docs.openstack.org/developer/tripleo-docs/index.html
+
+# suffix for deployment
 NUM=0
+# ready image for undercloud - using CentOS cloud image. just run and ssh into it.
 BASE_IMAGE="/var/lib/images/CentOS-7-x86_64-GenericCloud-1607.qcow2"
+# disk size for overcloud machines
 vm_disk_size="30G"
+# volume's poolname
 poolname="rdimages"
+net_driver=${net_driver:-e1000}
 
 source "$my_dir/functions"
 
+# cleanup previous domain
 delete_domains
 
+# create three networks (i don't know why external is needed)
 create_network management
 mgmt_net=`get_network_name management`
 create_network provisioning
@@ -19,23 +30,31 @@ prov_net=`get_network_name provisioning`
 create_network external
 ext_net=`get_network_name external`
 
+# create pool
 virsh pool-info $poolname &> /dev/null || create_pool $poolname
 pool_path=$(get_pool_path $poolname)
+# create root volumes for overcloud machines
 for i in {1..2} ; do
   virsh vol-delete overcloud-$NUM-$i.qcow2 --pool $poolname || rm -f $pool_path/overcloud-$NUM-$i.qcow2
   qemu-img create -f qcow2 -o preallocation=metadata $pool_path/overcloud-$NUM-$i.qcow2 $vm_disk_size
 done
+# copy image for undercloud and resize them
 cp $BASE_IMAGE $pool_path/undercloud-$NUM.qcow2
 qemu-img resize $pool_path/undercloud-$NUM.qcow2 +32G
 
+# define MAC's
 mgmt_ip=$(get_network_ip "management")
 mgmt_mac="00:16:00:00:0$NUM:02"
 prov_ip=$(get_network_ip "provisioning")
 prov_mac="00:16:00:00:0$NUM:06"
+# generate password/key for undercloud's root
 rm -f kp kp.pub
 ssh-keygen -b 2048 -t rsa -f "$my_dir/kp" -q -N ""
 rootpass=`openssl passwd -1 123`
 
+# TODO: use guestfish instead of manual attachment
+# mount undercloud root disk.
+# !!! WARNING !!! in case of errors you need to unmount/disconnect it manually!!!
 qemu-nbd -n -c /dev/nbd3 $pool_path/undercloud-$NUM.qcow2
 sleep 5
 tmpdir=$(mktemp -d)
@@ -59,13 +78,14 @@ sed -i "s root:\!\!: root:$rootpass: " $tmpdir/etc/shadow
 grep root $tmpdir/etc/shadow
 echo "PermitRootLogin yes" > $tmpdir/etc/ssh/sshd_config
 
+# unmount disk
 umount /dev/nbd3p1
 sleep 2
 rm -rf $tmpdir
 qemu-nbd -d /dev/nbd3
 sleep 2
 
-net_driver=${net_driver:-e1000}
+# define and start undercloud machine
 virt-install --name=rd-undercloud-$NUM \
   --ram=6144 \
   --vcpus=1,cores=1 \
@@ -80,6 +100,7 @@ virt-install --name=rd-undercloud-$NUM \
   --network network=$ext_net,model=$net_driver \
   --graphics vnc,listen=0.0.0.0
 
+# just define overcloud machines
 for i in {1..2} ; do
   virt-install --name rd-overcloud-$NUM-$i \
     --ram 8192 \
@@ -94,3 +115,8 @@ for i in {1..2} ; do
     --dry-run --print-xml > /tmp/oc-$NUM-$i.xml
   virsh define --file /tmp/oc-$NUM-$i.xml
 done
+
+# TODO: add timeout here
+# wait for undercloud machine
+truncate -s 0 ./tmp_file
+while ! scp -i kp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -B ./tmp_file root@192.168.172.2:/tmp/tmp_file ; do echo "Waiting for undercloud..." ; sleep 30 ; done
--- undercloud-install.sh
+++ undercloud-install.sh
@@ -1,33 +1,43 @@
 #!/bin/bash -ex
 
-#on kvm host:
+#on kvm host do once:
 #1) create stack user, create home directory, add him to libvirtd group
 
+# wait for undercloud and copy images for overcloud to it. (images can be build manually but it's too long - use previously built images)
 while ! scp -i kp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -B /home/stack/images.tar root@192.168.172.2:/tmp/images.tar ; do echo "Waiting for undercloud..." ; sleep 30 ; done
 
+# ssh into undercloud and run all next commands there
 ssh -i kp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@192.168.172.2
 
+# TODO: move all next code to another script file. copy it to undercloud and run it there.
+
+# allow ip forwarding
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
+# set static hostname
 hostnamectl set-hostname myhost.mydomain
 hostnamectl set-hostname --transient myhost.mydomain
 echo "127.0.0.1   localhost myhost myhost.mydomain" > /etc/hosts
 systemctl restart network
 
+# update OS
 yum update -y
+# create stack user
 useradd stack
 echo "stack:password" | chpasswd
 echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
 su - stack
 
-
-sudo yum install -y yum-utils screen
+# install useful utils
+sudo yum install -y yum-utils screen mc
+# add OpenStack repositories
 sudo curl -L -o /etc/yum.repos.d/delorean-mitaka.repo https://trunk.rdoproject.org/centos7-mitaka/current/delorean.repo
 sudo curl -L -o /etc/yum.repos.d/delorean-deps-mitaka.repo http://trunk.rdoproject.org/centos7-mitaka/delorean-deps.repo
+# install tripleo clients
 sudo yum -y install yum-plugin-priorities python-tripleoclient python-rdomanager-oscplugin
+# create undercloud configuration file. all IP addresses are relevant to create_env.sh script
 cp /usr/share/instack-undercloud/undercloud.conf.sample ~/undercloud.conf
-
 cat << EOF >> undercloud.conf
 [DEFAULT]
 local_ip = 192.168.176.1/24
@@ -42,20 +52,37 @@ network_gateway = 192.168.176.1
 discovery_iprange = 192.168.176.130,192.168.176.150
 EOF
 
+# install undercloud
 openstack undercloud install
 
-# build images or copy existing
+# function to build images if needed
+function create_images() {
+  mkdir -p ~/images
+  cd ~/images
+  export NODE_DIST=centos7
+  export USE_DELOREAN_TRUNK=1
+  export DELOREAN_REPO_FILE="delorean.repo"
+  #export DELOREAN_TRUNK_REPO="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-master-tripleo/"
+  export DELOREAN_TRUNK_REPO="http://trunk.rdoproject.org/centos7-mitaka/current/"
+  #export DIB_INSTALLTYPE_puppet_modules=source
+  openstack overcloud image build --all
+}
 
+# but right now script will use previously built images
+cd ~
 tar xvf /tmp/images.tar
-cd images
+
+# upload images to undercloud
+cd ~/images
 source ../stackrc
 openstack overcloud image upload
 cd ..
 
-
+# update undercloud's network information
 sid=`neutron subnet-list | awk '/ 192.168.176.0/{print $2}'`
 neutron subnet-update $sid --dns-nameserver 192.168.172.1
 
+# increase timeouts due to virtual installation
 sudo su -
 openstack-config --set /etc/nova/nova.conf DEFAULT rpc_response_timeout 600
 openstack-config --set /etc/ironic/ironic.conf DEFAULT rpc_response_timeout 600
@@ -63,11 +90,14 @@ openstack-service restart nova
 openstack-service restart ironic
 exit
 
+# copy ssh key from undercloud machine to KVM host. it needs to allow control of host VM's from undercloud's ironic service
+# TODO: this command needs to input password - rework it to batch mode
 ssh -i ~/.ssh/id_rsa stack@192.168.172.1 "echo $(cat ~/.ssh/id_rsa.pub) > .ssh/authorized_keys ; chmod 600 .ssh/authorized_keys"
+# ssh keys are in place - collect MAC addresses of overcloud machines
 for i in {1..2} ; do virsh -c qemu+ssh://stack@192.168.172.1/system domiflist rd-overcloud-0-$i | awk '$3 ~ "prov" {print $5};' ; done > /tmp/nodes.txt
 cat /tmp/nodes.txt
 
-
+# create overcloud machines definition
 jq . << EOF > ~/instackenv.json
 {
   "ssh-user": "stack",
@@ -105,36 +135,28 @@ jq . << EOF > ~/instackenv.json
   ]
 }
 EOF
+# check this json (it's optional)
 curl -O https://raw.githubusercontent.com/rthallisey/clapper/master/instackenv-validator.py
 python instackenv-validator.py -f instackenv.json
 
-
-source stackrc
+# re-define baremetal flavor
 openstack flavor delete baremetal || /bin/true
 openstack flavor create --id auto --ram 8192 --disk 28 --vcpus 2 baremetal
 openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" baremetal
 
+# import overcloud configuration
 openstack baremetal import --json instackenv.json
 openstack baremetal list
+# and configure overcloud
 openstack baremetal configure boot
 
+# do introspection - ironic will collect some hardware information from overcloud machines
 openstack baremetal introspection bulk start
+# this is a recommended command to check and wait end of introspection. but previous command can wait itself.
 #sudo journalctl -l -u openstack-ironic-discoverd -u openstack-ironic-discoverd-dnsmasq -u openstack-ironic-conductor -f
 
+# deploy overcloud. if you do it manually then I recommend to do it in screen.
 openstack overcloud deploy --templates --control-scale 1 --compute-scale 1 --neutron-tunnel-types vxlan --neutron-network-type vxlan
 
+# check status of deployment. other heat commands also is useful to check status.
 heat resource-list -n 5 overcloud
-
-
-
-function create_images() {
-  mkdir -p images
-  cd images
-  export NODE_DIST=centos7
-  export USE_DELOREAN_TRUNK=1
-  export DELOREAN_REPO_FILE="delorean.repo"
-  #export DELOREAN_TRUNK_REPO="http://buildlogs.centos.org/centos/7/cloud/x86_64/rdo-trunk-master-tripleo/"
-  export DELOREAN_TRUNK_REPO="http://trunk.rdoproject.org/centos7-mitaka/current/"
-  #export DIB_INSTALLTYPE_puppet_modules=source
-  openstack overcloud image build --all
-}
