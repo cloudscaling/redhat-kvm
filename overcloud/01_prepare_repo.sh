dd5b5b5949bf2482046785403b54ca042aa685db
Andrey Pavlov <andrey.mp@gmail.com>
2016-09-16 13:33:21 +0300

    rework data providing

--- overcloud/01_prepare_repo.sh
+++ overcloud/01_prepare_repo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
-url=$(hiera scaleio::packages_source_url)
+source /etc/scaleio.env
+url=$PackagesSourceURL
 echo "$url" >> /var/log/scaleio.log
 curl $url >> /var/log/scaleio.log
--- overcloud/02_install_puppets.sh
+++ overcloud/02_install_puppets.sh
@@ -1,3 +1,1 @@
 #!/bin/bash -e
-
-hiera scaleio::use_puppets_from_upstream >> /var/log/scaleio.log
