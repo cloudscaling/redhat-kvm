14e246a28a72ca3ce27f40f5d19c3e463562b9b6
Andrey Pavlov <andrey.mp@gmail.com>
2016-09-18 19:23:59 +0300

    fix second step - source env

--- overcloud/02_install_packages.sh
+++ overcloud/02_install_packages.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-puppet module install --version "<1.2.0" cloudscaling-scaleio
-puppet module install --version "<1.2.0" cloudscaling-scaleio_openstack
+source /etc/scaleio.env
+
+if [[ "$UsePuppetsFromUpstream" == "True" ]] ; then
+  for dep in puppetlabs-firewall puppetlabs-stdlib puppetlabs-inifile ; do
+    puppet module install $dep || /bin/true
+  done
+  rm -rf /etc/puppet/modules/scaleio
+  git clone -q https://github.com/emccode/puppet-scaleio /etc/puppet/modules/scaleio
+  rm -rf /etc/puppet/modules/scaleio_openstack
+  git clone -q https://github.com/emccode/puppet-scaleio-openstack /etc/puppet/modules/scaleio_openstack
+else
+  puppet module install --version "<1.2.0" cloudscaling-scaleio_openstack
+  # NOTE: this module can't be installed due to strange installed modules
+  # TODO: fix it...
+  puppet module install --version "<1.2.0" cloudscaling-scaleio
+fi
 
 function server-cmd {
   puppet apply -e "$1" --detailed-exitcodes
@@ -27,7 +41,7 @@ elif [[ "$role" == "novacompute" ]] ; then
 
   server-cmd "class { 'scaleio::sdc_server': ftp=>'$ScaleIODriverFTP' }"
 
-  if [[ $InstallSDSToCompute == 'true' ]] ; then
+  if [[ $InstallSDSToCompute == 'True' ]] ; then
     server-cmd "class { 'scaleio::sds_server': ftp=>'$ScaleIODriverFTP' }"
   fi
 
