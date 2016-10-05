ac5d3297b459500fdb398b0f4c20feae6f45c7a4
Andrey Pavlov <andrey.mp@gmail.com>
2016-10-05 22:17:59 +0300

    revert to single template model

---
+++ overcloud/01_prepare_repo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+source /etc/scaleio.env
+
+DEST='/var/lib/scaleio/repo'
+mkdir -p $DEST
+
+packages=`curl --silent $PackagesSourceURL | grep -o 'EMC-ScaleIO-[_a-zA-Z0-9\.\-]*rpm' | sort | uniq`
+for p in $packages ; do
+  if [[ ! -f "$DEST/$p" || ! -z "${FORCE_DOWNLOAD+x}" ]] ; then
+    wget -P "$DEST/" "${PackagesSourceURL}${p}"
+  fi
+done
+
+yum install -y createrepo
+createrepo -v $DEST/
+
+cat << EOF > /etc/yum.repos.d/scaleio.repo
+[scaleio]
+name=Local ScaleIO
+baseurl=file://$DEST
+gpgcheck=0
+enabled=1
+EOF
