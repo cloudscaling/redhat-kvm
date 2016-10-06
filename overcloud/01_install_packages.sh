a74c7bc842cc61dff18d7933955c77e4fa0d2fe1
Andrey Pavlov <andrey.mp@gmail.com>
2016-10-06 17:27:34 +0300

    rework scripts for new AllNodesExtraConfig hook

---
+++ overcloud/01_install_packages.sh
@@ -0,0 +1,75 @@
+#!/bin/bash -e
+
+source /etc/scaleio.env
+
+DEST='/var/lib/scaleio/repo'
+mkdir -p $DEST
+
+packages=`curl --silent $PackagesSourceURL | grep -o 'EMC-ScaleIO-[_a-zA-Z0-9\.\-]*rpm' | sort | uniq`
+for p in $packages ; do
+  if [[ ! -f "$DEST/$p" || ! -z "${FORCE_DOWNLOAD+x}" ]] ; then
+    wget -P "$DEST/" "${PackagesSourceURL}${p}"
+  fi
+done
+
+yum install -y createrepo
+createrepo -v $DEST/
+
+cat << EOF > /etc/yum.repos.d/scaleio.repo
+[scaleio]
+name=Local ScaleIO
+baseurl=file://$DEST
+gpgcheck=0
+enabled=1
+EOF
+
+
+if [[ "$UsePuppetsFromUpstream" == "True" ]] ; then
+  for dep in puppetlabs-firewall puppetlabs-stdlib puppetlabs-inifile ; do
+    puppet module install $dep || /bin/true
+  done
+  rm -rf /etc/puppet/modules/scaleio
+  git clone -q https://github.com/emccode/puppet-scaleio /etc/puppet/modules/scaleio
+  rm -rf /etc/puppet/modules/scaleio_openstack
+  git clone -q https://github.com/emccode/puppet-scaleio-openstack /etc/puppet/modules/scaleio_openstack
+else
+  puppet module install --version "<1.2.0" cloudscaling-scaleio_openstack
+  # NOTE: this module can't be installed due to strange installed modules
+  # TODO: fix it...
+  puppet module install --version "<1.2.0" cloudscaling-scaleio
+fi
+
+function server-cmd() {
+  set +e
+  puppet apply -e "$1" --detailed-exitcodes
+  local exit_code=$?
+  set -e
+  if [[ $exit_code != 0 && $exit_code != 2 ]]; then
+    echo "The run failed. Exit code is $exit_code."
+    exit 1
+  fi
+}
+
+if [[ "$role" == "controller" ]] ; then
+
+  server-cmd "class { 'scaleio::mdm_server': }"
+
+  # TODO: support haproxy for gateway
+  api_port=$GatewayPort
+  server-cmd "class { 'scaleio::gateway_server': port=>'$api_port' }"
+
+  server-cmd "class { 'scaleio::gui_server': }"
+
+elif [[ "$role" == "novacompute" ]] ; then
+
+  server-cmd "class { 'scaleio::sdc_server': ftp=>'$ScaleIODriverFTP' }"
+
+elif [[ "$role" == "blockstorage" ]] ; then
+
+  server-cmd "class { 'scaleio::sdc_server': ftp=>'$ScaleIODriverFTP' }"
+
+fi
+
+if [[ $RolesForSDS =~ $role ]] ; then
+  server-cmd "class { 'scaleio::sds_server': ftp=>'$ScaleIODriverFTP' }"
+fi
--- overcloud/03_configure_cluster.sh
+++ overcloud/03_configure_cluster.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# NOTE: this script is run only for first controller
+# TODO: rework script runner for registering other managers and tie-breakers (is_manager = 1 or 0)
+# server-cmd "class { 'scaleio::mdm_server': is_manager=>$is_manager }"
+
 # Network mapping:
 #   1. Internal API (OpenStack internal API, RPC, and DB):
 #     - MDM/SDS/SDC <==> MDM
@@ -40,100 +44,96 @@ function is_in_list() {
 }
 
 cloud_name=$(hostname | cut -d '-' -f 1)
+controllers_internal_ips=`awk "/${cloud_name}-controller-[0-9]+-internalapi$/ {print(\$1)}" /etc/hosts | tr '\r\n' ',' | sed 's/,$//g'`
 
-# these variable are a comma separated list
-names="$(hiera controller_node_names)"
-controllers_internal_ips=$(awk "/${cloud_name}-controller-[0-9]+-internalapi$/ {print(\$1)}" /etc/hosts | tr '\r\n' ',' | sed 's/,$//g')
+# NOTE: at this moment all nodes was installed and we can configure cluster
+export FACTER_mdm_ips="$controllers_internal_ips"
 
+# Register protection domains and storage pools
+protection_domains_array=($(echo ${ProtectionDomain:-''} | sed 's/,/ /g'))
+storage_pools_list=${StoragePools:-''}
+storage_pools_array=($(echo $storage_pools_list | sed 's/,/ /g'))
+fault_sets_list='' # TODO: pass correct fault sets
+for pd in ${protection_domains_array[@]} ; do
+  pd_opts="sio_name=>'$pd'"
+  if [[ -n "$storage_pools_list" ]] ; then
+    sps="'$(echo $storage_pools_list | sed 's/,/'\'','\''/g')'"
+    pd_opts+=", storage_pools=>[$sps]"
+  fi
+  if [[ -n "$fault_sets_list" ]] ; then
+    fss="'$(echo $fault_sets_list | sed 's/,/'\'','\''/g')'"
+    pd_opts+=", fault_sets=>[$fss]"
+  fi
+  cluster-cmd "scaleio::protection_domain { 'protection domain $pd': $pd_opts }"
+  for sp in ${storage_pools_array[@]} ; do
+    sp_opts="sio_name=>'$sp', protection_domain=>'$pd', checksum_mode=>"
+    if [[ $ChecksumMode == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
+    sp_opts+=", scanner_mode=>"
+    if [[ $ScannerMode == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
+    sp_opts+=", zero_padding_policy=>"
+    if [[ $ZeroPadding == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
+    sp_opts+=", spare_percentage=>$SparePolicy"
+    sp_opts+=", rfcache_usage=>"
+    if is_in_list $sp "$RFCacheCachedPools" ; then sp_opts+="'use'"; else sp_opts+="'dont_use'"; fi
+    sp_opts+=", rmcache_usage=>"
+    if is_in_list $sp "$RMCacheCachedPools" ; then
+      sp_opts+="'use', rmcache_write_handling_mode=>'cached'"
+    elif is_in_list $sp "$RMCachePassthroughPools" ; then
+      sp_opts+="'use', rmcache_write_handling_mode=>'passthrough'"
+    else
+      sp_opts+="'dont_use'"
+    fi
+    cluster-cmd "scaleio::storage_pool { 'storage pool $sp': $sp_opts }"
+  done
+done
 
-role=$(hostname | cut -d '-' -f 2)
-if [[ "$role" == "controller" ]] ; then
-
-  node_suffix=$(hostname | cut -d '-' -f 3)
-  # TODO: get index of node by suffix from $names
-  node_index="$node_suffix"
-  if [[ $node_index == 0 ]] ; then
-
-    # NOTE: at this moment all nodes was installed and we can configure cluster
-    export FACTER_mdm_ips="$controllers_internal_ips"
-    
-    # Install and configure Gateways
-    server-cmd "class { 'scaleio::gateway_server': mdm_ips=>'$controllers_internal_ips' }"
-
-    # TODO: add standby mdms if needed
-    #cluster-cmd "scaleio::mdm { 'mdm $node': sio_name=>'$name', ips=>'$internal_ip', role=>'$role', management_ips=>$management_ip }"
-
-    # Register protection domains and storage pools
-    protection_domains_array=($(echo ${ProtectionDomain:-''} | sed 's/,/ /g'))
-    storage_pools_list=${StoragePools:-''}
-    storage_pools_array=($(echo $storage_pools_list | sed 's/,/ /g'))
-    fault_sets_list='' # TODO: pass correct fault sets
-    for pd in ${protection_domains_array[@]} ; do
-      pd_opts="sio_name=>'$pd'"
-      if [[ -n "$storage_pools_list" ]] ; then
-        sps="'$(echo $storage_pools_list | sed 's/,/'\'','\''/g')'"
-        pd_opts+=", storage_pools=>[$sps]"
-      fi
-      if [[ -n "$fault_sets_list" ]] ; then
-        fss="'$(echo $fault_sets_list | sed 's/,/'\'','\''/g')'"
-        pd_opts+=", fault_sets=>[$fss]"
-      fi
-      cluster-cmd "scaleio::protection_domain { 'protection domain $pd': $pd_opts }"
-      for sp in ${storage_pools_array[@]} ; do
-        sp_opts="sio_name=>'$sp', protection_domain=>'$pd', checksum_mode=>"
-        if [[ $ChecksumMode == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
-        sp_opts+=", scanner_mode=>"
-        if [[ $ScannerMode == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
-        sp_opts+=", zero_padding_policy=>"
-        if [[ $ZeroPadding == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
-        sp_opts+=", spare_percentage=>$SparePolicy"
-        sp_opts+=", rfcache_usage=>"
-        if is_in_list $sp "$RFCacheCachedPools" ; then sp_opts+="'use'"; else sp_opts+="'dont_use'"; fi
-        sp_opts+=", rmcache_usage=>"
-        if is_in_list $sp "$RMCacheCachedPools" ; then
-          sp_opts+="'use', rmcache_write_handling_mode=>'cached'"
-        elif is_in_list $sp "$RMCachePassthroughPools" ; then
-          sp_opts+="'use', rmcache_write_handling_mode=>'passthrough'"
-        else
-          sp_opts+="'dont_use'"
-        fi
-        cluster-cmd "scaleio::storage_pool { 'storage pool $sp': $sp_opts }"
-      done
-    done
-
-    # Register SDS nodes
-    # get somewhere a list of all nodes
-    # this hack is for Mitaka. for Newton we can get it from hiera (service_node_names)
-    device_paths_list=${DevicePaths:-''}
-    nodes=`grep -o "${cloud_name}-[a-zA-Z]\+-[0-9]\+\$" /etc/hosts`
-    for node in $nodes ; do
-      role=$(echo $node | cut -d '-' -f 2)
-      if [[ $RolesForSDS =~ $role ]] ; then
-        sds_opts="sio_name=>'$node', protection_domain=>'$pd'"
-        if [[ -n "$storage_pools_list" && -n "$device_paths_list" ]] ;then
-          sds_opts+=", storage_pools=>'$storage_pools_list', device_paths=>'$device_paths_list'"
-        fi
-        if [[ -n "$RFCacheDevices" ]] ; then
-          sds_opts+=", rfcache_devices=>'$RFCacheDevices'"
-        fi
-        node_storage_api_ip=$(awk "/${node}-storage\$/ {print(\$1)}" /etc/hosts)
-        node_storagemgmt_api_ip=$(awk "/${node}-storagemgmt\$/ {print(\$1)}" /etc/hosts)
-        if [[ "$node_storage_api_ip" != "$node_storagemgmt_api_ip" ]] ; then
-          sds_opts+=", ips=>'$node_storage_api_ip,$node_storagemgmt_api_ip', ip_roles=>'sdc_only,sds_only'"
-        else
-          sds_opts+=", ips=>'$node_storage_api_ip', ip_roles=>'all'"
-        fi
-        cluster-cmd "scaleio::sds { '$node': $sds_opts }"
-      fi
-    done
-  fi  # if [[ $node_index == 0 ]] ; then
-
-elif [[ "$role" == "novacompute" ]] ; then
-
-  server-cmd "class { 'scaleio::sdc_server': mdm_ip=>'$controllers_internal_ips' }"
-
-elif [[ "$role" == "blockstorage" ]] ; then
-
-  server-cmd "class { 'scaleio::sdc_server': mdm_ip=>'$controllers_internal_ips' }"
-
-fi
+# Register SDS nodes
+# get somewhere a list of all nodes
+# this hack is for Mitaka. for Newton we can get it from hiera (service_node_names)
+device_paths_list=${DevicePaths:-''}
+nodes=`grep -o "${cloud_name}-[a-zA-Z]\+-[0-9]\+\$" /etc/hosts`
+for node in $nodes ; do
+  role=$(echo $node | cut -d '-' -f 2)
+  if [[ $RolesForSDS =~ $role ]] ; then
+    sds_opts="sio_name=>'$node', protection_domain=>'$pd'"
+    if [[ -n "$storage_pools_list" && -n "$device_paths_list" ]] ;then
+      sds_opts+=", storage_pools=>'$storage_pools_list', device_paths=>'$device_paths_list'"
+    fi
+    if [[ -n "$RFCacheDevices" ]] ; then
+      sds_opts+=", rfcache_devices=>'$RFCacheDevices'"
+    fi
+    node_storage_api_ip=$(awk "/${node}-storage\$/ {print(\$1)}" /etc/hosts)
+    node_storagemgmt_api_ip=$(awk "/${node}-storagemgmt\$/ {print(\$1)}" /etc/hosts)
+    if [[ "$node_storage_api_ip" != "$node_storagemgmt_api_ip" ]] ; then
+      sds_opts+=", ips=>'$node_storage_api_ip,$node_storagemgmt_api_ip', ip_roles=>'sdc_only,sds_only'"
+    else
+      sds_opts+=", ips=>'$node_storage_api_ip', ip_roles=>'all'"
+    fi
+    cluster-cmd "scaleio::sds { '$node': $sds_opts }"
+  fi
+  if [[ -n "$fault_sets_list" ]] ; then
+    fss="'$(echo $fault_sets_list | sed 's/,/'\'','\''/g')'"
+    pd_opts+=", fault_sets=>[$fss]"
+  fi
+  cluster-cmd "scaleio::protection_domain { 'protection domain $pd': $pd_opts }"
+  for sp in ${storage_pools_array[@]} ; do
+    sp_opts="sio_name=>'$sp', protection_domain=>'$pd', checksum_mode=>"
+    if [[ $ChecksumMode == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
+    sp_opts+=", scanner_mode=>"
+    if [[ $ScannerMode == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
+    sp_opts+=", zero_padding_policy=>"
+    if [[ $ZeroPadding == "True" ]] ; then sp_opts+="'enable'"; else sp_opts+="'disable'"; fi
+    sp_opts+=", spare_percentage=>$SparePolicy"
+    sp_opts+=", rfcache_usage=>"
+    if is_in_list $sp "$RFCacheCachedPools" ; then sp_opts+="'use'"; else sp_opts+="'dont_use'"; fi
+    sp_opts+=", rmcache_usage=>"
+    if is_in_list $sp "$RMCacheCachedPools" ; then
+      sp_opts+="'use', rmcache_write_handling_mode=>'cached'"
+    elif is_in_list $sp "$RMCachePassthroughPools" ; then
+      sp_opts+="'use', rmcache_write_handling_mode=>'passthrough'"
+    else
+      sp_opts+="'dont_use'"
+    fi
+    cluster-cmd "scaleio::storage_pool { 'storage pool $sp': $sp_opts }"
+  done
+done
