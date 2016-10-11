50d5efee233360b6e6342c3393ee83c64f2b5625
Andrey Pavlov <andrey.mp@gmail.com>
2016-10-11 23:16:37 +0300

    use piblic_vip instead of loadbalancer from hiera

--- overcloud/04_configure_controllers.sh
+++ overcloud/04_configure_controllers.sh
@@ -16,11 +16,10 @@ cloud_name=$(hostname | cut -d '-' -f 1)
 controllers_internal_ips=`grep "${cloud_name}-controller-[0-9]\+-internalapi$" /etc/hosts | awk '{print($1)}' | tr '\r\n' ',' | sed 's/,$//g'`
 
 # Configure Gateway HA
-gateway_vip=$(hiera 'tripleo::loadbalancer::internal_api_virtual_ip')
-if [[ "$gateway_vip" != 'nil' ]] ; then
+if [[ -n "$public_vip" ]] ; then
   (( int_api_port=api_port-1 ))
   server-cmd "class { 'scaleio::gateway_server': port=>'$int_api_port', mdm_ips=>'$controllers_internal_ips' }"
-  listen_opts="bind => { '${gateway_vip}:${api_port}' => [], }"
+  listen_opts="bind => { '${public_vip}:${api_port}' => [], }"
   listen_opts+=", options => { 'balance' => 'roundrobin', 'mode' => 'tcp', 'option' => ['tcplog'], }"
   listen_opts+=", collect_exported => false"
   balance_opts="listening_service => 'scaleio-gateway'"
@@ -31,7 +30,7 @@ if [[ "$gateway_vip" != 'nil' ]] ; then
   server_names="'$(echo $controllers_names | sed 's/,/'\'','\''/g')'"
   balance_opts+=", server_names => [$server_names]"
   balance_opts+=", options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3'"
-  server-cmd "include ::haproxy haproxy::listen { 'scaleio-gateway': ${listen_opts} }->haproxy::balancermember { 'scaleio-gateway': ${balance_opts} }"
+  server-cmd "include ::haproxy haproxy::listen { 'scaleio-gateway': ${listen_opts} } -> haproxy::balancermember { 'scaleio-gateway': ${balance_opts} }"
 else
-  server-cmd "class { 'scaleio::gateway_server': port=>'$api_port', mdm_ips=>'$controllers_internal_ips' }"  
+  server-cmd "class { 'scaleio::gateway_server': port=>'$api_port', mdm_ips=>'$controllers_internal_ips' }"
 fi
--- overcloud/05_configure_clients.sh
+++ overcloud/05_configure_clients.sh
@@ -14,11 +14,11 @@ function server-cmd() {
 cloud_name=$(hostname | cut -d '-' -f 1)
 controllers_internal_ips=`grep "${cloud_name}-controller-[0-9]\+-internalapi$" /etc/hosts | awk '{print($1)}' | tr '\r\n' ',' | sed 's/,$//g'`
 
+# install SDC to all machines.
 server-cmd "class { 'scaleio::sdc_server': mdm_ip=>'$controllers_internal_ips' }"
 
 # TODO: run other steps accccording to $role
 gateway_port=${GatewayPort:-4443}
-gateway_vip=$(hiera 'tripleo::loadbalancer::internal_api_virtual_ip')
 
 if [[ "$role" == "controller" ]] ; then
 
@@ -32,7 +32,7 @@ elif [[ "$role" == "novacompute" ]] ; then
   # TODO: think about passing all protection domains instead of first
   server-cmd "class { 'scaleio_openstack::nova':
     gateway_user => 'scaleio_client', gateway_password => '$ScaleIOClientPassword',
-    gateway_ip => '$gateway_vip', gateway_port => '$gateway_port',
+    gateway_ip => '$public_vip', gateway_port => '$gateway_port',
     protection_domains => '$ProtectionDomain', storage_pools => '$StoragePools',
     provisioning_type => '$ProvisioningType',
   }"
@@ -42,7 +42,7 @@ elif [[ "$role" == "blockstorage" ]] ; then
   # TODO: think about passing all protection domains instead of first
   server-cmd "class { 'scaleio_openstack::cinder':
     gateway_user => 'scaleio_client', gateway_password => '$ScaleIOClientPassword',
-    gateway_ip => '$gateway_vip', gateway_port => '$gateway_port',
+    gateway_ip => '$public_vip', gateway_port => '$gateway_port',
     protection_domains => '$ProtectionDomain', storage_pools => '$StoragePools',
     provisioning_type => '$ProvisioningType',
   }"
