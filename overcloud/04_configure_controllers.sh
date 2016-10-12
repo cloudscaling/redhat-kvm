3196a3960eb3db3c36b8dd7d8956840e6cb460a0
Andrey Pavlov <andrey.mp@gmail.com>
2016-10-12 12:40:28 +0300

    switch off haproxy for gateway
    it rewrites main haproxy configuration file and OpenStack doesn't work

--- overcloud/04_configure_controllers.sh
+++ overcloud/04_configure_controllers.sh
@@ -15,22 +15,25 @@ api_port=${GatewayPort:-4443}
 cloud_name=$(hostname | cut -d '-' -f 1)
 controllers_internal_ips=`grep "${cloud_name}-controller-[0-9]\+-internalapi$" /etc/hosts | awk '{print($1)}' | tr '\r\n' ',' | sed 's/,$//g'`
 
+# TODO: rework this. right now it rewrites haproxy configuration.
 # Configure Gateway HA
-if [[ -n "$public_vip" ]] ; then
-  (( int_api_port=api_port-1 ))
-  server-cmd "class { 'scaleio::gateway_server': port=>'$int_api_port', mdm_ips=>'$controllers_internal_ips' }"
-  listen_opts="bind => { '${public_vip}:${api_port}' => [], }"
-  listen_opts+=", options => { 'balance' => 'roundrobin', 'mode' => 'tcp', 'option' => ['tcplog'], }"
-  listen_opts+=", collect_exported => false"
-  balance_opts="listening_service => 'scaleio-gateway'"
-  balance_opts+=", ports => '${int_api_port}'"
-  ipaddresses="'$(echo $controllers_internal_ips | sed 's/,/'\'','\''/g')'"
-  balance_opts+=", ipaddresses => [$ipaddresses]"
-  controllers_names="$(hiera controller_node_names)"
-  server_names="'$(echo $controllers_names | sed 's/,/'\'','\''/g')'"
-  balance_opts+=", server_names => [$server_names]"
-  balance_opts+=", options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3'"
-  server-cmd "include ::haproxy haproxy::listen { 'scaleio-gateway': ${listen_opts} } -> haproxy::balancermember { 'scaleio-gateway': ${balance_opts} }"
-else
-  server-cmd "class { 'scaleio::gateway_server': port=>'$api_port', mdm_ips=>'$controllers_internal_ips' }"
-fi
+#if [[ -n "$public_vip" ]] ; then
+#  (( int_api_port=api_port-1 ))
+#  server-cmd "class { 'scaleio::gateway_server': port=>'$int_api_port', mdm_ips=>'$controllers_internal_ips' }"
+#  listen_opts="bind => { '${public_vip}:${api_port}' => [], }"
+#  listen_opts+=", options => { 'balance' => 'roundrobin', 'mode' => 'tcp', 'option' => ['tcplog'], }"
+#  listen_opts+=", collect_exported => false"
+#  balance_opts="listening_service => 'scaleio-gateway'"
+#  balance_opts+=", ports => '${int_api_port}'"
+#  ipaddresses="'$(echo $controllers_internal_ips | sed 's/,/'\'','\''/g')'"
+#  balance_opts+=", ipaddresses => [$ipaddresses]"
+#  controllers_names="$(hiera controller_node_names)"
+#  server_names="'$(echo $controllers_names | sed 's/,/'\'','\''/g')'"
+#  balance_opts+=", server_names => [$server_names]"
+#  balance_opts+=", options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3'"
+#  server-cmd "include ::haproxy haproxy::listen { 'scaleio-gateway': ${listen_opts} } -> haproxy::balancermember { 'scaleio-gateway': ${balance_opts} }"
+#else
+#  server-cmd "class { 'scaleio::gateway_server': port=>'$api_port', mdm_ips=>'$controllers_internal_ips' }"
+#fi
+
+server-cmd "class { 'scaleio::gateway_server': port=>'$api_port', mdm_ips=>'$controllers_internal_ips' }"
