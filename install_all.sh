00c99a81057618a2f4b676777c6f4440839fe84e
Andrey Pavlov <andrey.mp@gmail.com>
2016-10-17 19:29:57 +0300

    fix passing puppets_version to overcloud

--- install_all.sh
+++ install_all.sh
@@ -12,3 +12,7 @@ ip_addr="192.168.${env_addr}.2"
 ssh_opts="-i $my_dir/kp-$NUM -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 ssh_addr="root@${ip_addr}"
 ssh -t $ssh_opts $ssh_addr "sudo -u stack NUM=$NUM DEPLOY=1 /home/stack/overcloud-install.sh"
+
+if [[ -n "$SUDO_USER" ]] ; then
+  chown $SUDO_USER "$my_dir/kp-$NUM"
+fi
