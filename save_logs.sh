e691321c20e51cf01d3a7f6c87e6b6b5be602d5a
Andrey Pavlov <andrey.mp@gmail.com>
2016-10-25 12:39:59 +0300

    tripleo: add scaleio logs to artifacts

--- save_logs.sh
+++ save_logs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
@@ -19,6 +19,8 @@ for mid in `nova list | awk '/overcloud/{print $4"+"$12}'` ; do
   mn="`echo $mid | cut -d '+' -f 1`"
   mip="`echo $mid | cut -d '=' -f 2`"
   echo "INFO: save logs from machine $mn ($mip)"
-  ssh heat-admin@$mip sudo tar cf logs.tar /var/log/nova /var/log/cinder /var/log/glance /etc/nova /etc/cinder /etc/glance /etc/scaleio.env /var/log/scaleio.log
+  ssh heat-admin@$mip sudo tar --ignore-failed-read -cf logs.tar /var/log/nova /var/log/cinder /var/log/glance /etc/nova /etc/cinder /etc/glance /etc/scaleio.env /var/log/scaleio.log
+  files=`ssh heat-admin@$mip sudo find /opt/ \( -name logs -o -name conf \)`
+  ssh heat-admin@$mip sudo tar --ignore-failed-read -rf logs.tar $files
   scp heat-admin@$mip:logs.tar $mn-logs.tar
 done
