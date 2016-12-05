1717deaf351e2c0c15d4bee10d0a9f63c4b6e826
alexey-mr <alexey.morlang@gmail.com>
2016-12-05 15:08:54 +0300

    haproxy notify after config changes

--- overcloud/04_configure_controllers.sh
+++ overcloud/04_configure_controllers.sh
@@ -19,9 +19,11 @@ use_load_balancer=`hiera enable_load_balancer`
 # Configure Gateway HA
 if [[ -n "$public_vip" && "$use_load_balancer" == "true" ]] ; then
   (( int_api_port=api_port-1 ))
-  controllers_names="$(hiera controller_node_names)"
-  server_names="'$(echo $controllers_names | sed 's/,/'\'','\''/g')'"
-  ipaddresses="'$(echo $controllers_internal_ips | sed 's/,/'\'','\''/g')'"
+  controllers_names=`grep 'overcloud-controller-[0-9]\+[-\.]internalapi$' /etc/hosts | grep -o 'overcloud-controller-[0-9]\+' | tr '\r\n' ',' | sed 's/,$//g'`
+  main_server_name="'$(echo $controllers_names | cut -d ',' -f1 | sed 's/,/'\'','\''/g')'"
+  backup_server_names="'$(echo $controllers_names | cut -d ',' -f2- | sed 's/,/'\'','\''/g')'"
+  main_server_ip="'$(echo $controllers_internal_ips | cut -d ',' -f1 | sed 's/,/'\'','\''/g')'"
+  backup_server_ips="'$(echo $controllers_internal_ips | cut -d ',' -f2- | sed 's/,/'\'','\''/g')'"
   #TODO: tmp hack, we should not call tripleo::loadbalancer.
   # Ideally it should be in the .yaml & tripleo puppet lib
   cmd="\
@@ -37,23 +39,27 @@ if [[ -n "$public_vip" && "$use_load_balancer" == "true" ]] ; then
     haproxy::balancermember { 'scaleio-gateway': \
       listening_service => 'scaleio-gateway', \
       ports => ${int_api_port}, \
-      ipaddresses => [${ipaddresses}], \
-      server_names => [${server_names}], \
+      ipaddresses => [$main_server_ip], \
+      server_names => [$main_server_name], \
+      options => 'check inter 10s fastinter 2s downinter 3s rise 3 fall 3', \
+    } -> \
+    haproxy::balancermember { 'scaleio-gateway-backup': \
+      listening_service => 'scaleio-gateway', \
+      ports => ${int_api_port}, \
+      ipaddresses => [$backup_server_ips], \
+      server_names => [$backup_server_names], \
       options => 'backup check inter 10s fastinter 2s downinter 3s rise 3 fall 3', \
     } -> \
     class { '::tripleo::loadbalancer' : \
-      controller_hosts       => [${ipaddresses}], \
-      controller_hosts_names => [${server_names}], \
+      controller_hosts       => [$main_server_ip,$backup_server_ips], \
+      controller_hosts_names => [$main_server_name,$backup_server_names], \
       manage_vip             => false, \
       mysql_clustercheck     => true, \
       haproxy_service_manage => false, \
-    } ~> \
-    service { 'haproxy': \
-      ensure => 'running', \
-    }  \
+    }
   "
   server-cmd "$cmd"
-  
+  service haproxy restart
 else
   server-cmd "class { 'scaleio::gateway_server': port=>'$api_port', mdm_ips=>'$controllers_internal_ips' }"
 fi
